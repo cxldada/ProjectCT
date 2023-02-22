f05a443273461d3b9ab2cea8870e4ed26f960217
cxl <c.x.l@live.com>
2023-02-22 22:41:28 +0800

    测试spdlog日志

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,13 @@
 #include <iostream>
+#include "spdlog/spdlog.h"
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello CT!" << std::endl;
+    spdlog::set_level(spdlog::level::trace);
+    spdlog::error("{}", "hellow ct spdlog error");
+    spdlog::warn("{}", "hellow ct spdlog warn");
+    spdlog::info("{}", "hellow ct spdlog info");
+    spdlog::debug("{}", "hellow ct spdlog debug");
+    spdlog::trace("{}", "hellow ct spdlog trace");
 
     return 0;
 }
